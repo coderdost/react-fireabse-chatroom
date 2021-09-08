b014ebb78c5356a1d1e50d6511b550135a44d75f
Abhishek Rathore <rathore.abhishek87@gmail.com>
2021-09-08 15:00:22 +0530

    chat room completed

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAH6gc_UYpTbBiwrfpXCCigM1iaouE-t2k",
+  authDomain: "react-chat-app-cf5f5.firebaseapp.com",
+  databaseURL: "https://react-chat-app-cf5f5-default-rtdb.firebaseio.com",
+  projectId: "react-chat-app-cf5f5",
+  storageBucket: "react-chat-app-cf5f5.appspot.com",
+  messagingSenderId: "405755936751",
+  appId: "1:405755936751:web:5272d5078f495c1b13001a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
