1c45c2b18b7001add8b5a2673f409ea61d43b1c1
Abhishek Rathore <rathore.abhishek87@gmail.com>
2021-09-09 13:28:33 +0530

    adding social login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import { useEffect, useState } from 'react';
 import { getDatabase, push, ref, set,onChildAdded } from "firebase/database";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
 import './App.css';
 
 function App() {
-  const [name, setName] = useState('');
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+
+  const googleLogin = () =>{
+    signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    setUser({name:result.user.displayName, email: result.user.email})
+    console.log(token, user);
+
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+  }
+
+  const [user, setUser] = useState('');
   const [chats, setChats] = useState([]);
   const [msg, setMsg] = useState('');
 
   const db = getDatabase();
   const chatListRef = ref(db, 'chats');
+
+
 
   const updateHeight=()=>{
     const el = document.getElementById('chat');
@@ -33,26 +62,27 @@ function App() {
 
     const chatRef = push(chatListRef);
     set(chatRef, {
-      name, message: msg 
+      user, message: msg 
     });
     setMsg('');
   };
   return (
     <div>
-      {name? null: <div>
-        <input
+      {user.email? null: <div>
+        {/* <input
           type="text"
-          placeholder="Enter name to start"
-          onBlur={(e) => setName(e.target.value)}
-        ></input>
+          placeholder="Enter user to start"
+          onBlur={(e) => setUser(e.target.value)}
+        ></input> */}
+        <button onClick={e=>{googleLogin()}}>Google SignIn</button>
       </div>}
-   { name? <div>
-      <h3>User: {name}</h3>
+   { user.email? <div>
+      <h3>User: {user.name}</h3>
       <div id="chat" className="chat-container">
         {chats.map((c,i) => (
-          <div key={i} className={`container ${c.name === name ? 'me' : ''}`}>
+          <div key={i} className={`container ${c.user.email === user.email ? 'me' : ''}`}>
             <p className="chatbox">
-              <strong>{c.name}: </strong>
+              <strong>{c.user.name}: </strong>
               <span>{c.message}</span>
             </p>
           </div>
